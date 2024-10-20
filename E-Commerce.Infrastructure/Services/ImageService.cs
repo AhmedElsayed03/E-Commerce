5cf3cb8da943512f858a1a88acdbcffca229a885
Ahmed Elsayesd <aehamoda@gmail.com>
2024-10-21 02:10:41 +0300

    Added Services (Product/Image)

---
+++ E-Commerce.Infrastructure/Services/ImageService.cs
@@ -0,0 +1,20 @@
+﻿using E_Commerce.Application.Abstractions.Repositories;
+using E_Commerce.Application.Abstractions.Services;
+using E_Commerce.Application.Abstractions.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infrastructure.Services
+{
+    public class ImageService:IImageService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ImageService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
---
+++ E-Commerce.Infrastructure/Services/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using E_Commerce.Application.Abstractions.Services;
+using E_Commerce.Application.Abstractions.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infrastructure.Services
+{
+    public class ProductService:IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
